426d73d4365fbfbd5f60a43535e840ccbab7dc37
myluki2000 <lukas636@live.com>
2022-02-20 23:36:35 +0100

    Projektdateien hinzufügen.

---
+++ PeridotWindows/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PeridotWindows
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new PeridotEngine.Main())
+                game.Run();
+        }
+    }
+}

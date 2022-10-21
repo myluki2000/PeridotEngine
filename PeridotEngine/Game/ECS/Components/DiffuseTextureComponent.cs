a29a1273464ba4efaacd509f21397ecf05aa0839
myluki2000 <lukas636@live.com>
2022-10-21 17:25:16 +0200

    Added DiffuseTextureComponent

---
+++ PeridotEngine/Game/ECS/Components/DiffuseTextureComponent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PeridotEngine.Game.ECS.Components
+{
+    internal class TextureComponent : IComponent
+    {
+        public Texture2D Texture;
+    }
+}

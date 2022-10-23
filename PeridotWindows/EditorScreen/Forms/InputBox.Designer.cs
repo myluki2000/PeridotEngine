82d3ce2f51259252813e3b2a516d570b1781595e
myluki2000 <lukas636@live.com>
2022-10-23 16:41:56 +0200

    InputBox

---
+++ PeridotWindows/EditorScreen/Forms/InputBox.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace PeridotWindows.EditorScreen.Forms
+{
+    partial class InputBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.lbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOk
+            // 
+            this.btnOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOk.Location = new System.Drawing.Point(248, 84);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 0;
+            this.btnOk.Text = "Ok";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.Location = new System.Drawing.Point(329, 84);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // tbInput
+            // 
+            this.tbInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbInput.Location = new System.Drawing.Point(12, 55);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(392, 23);
+            this.tbInput.TabIndex = 2;
+            // 
+            // lbl
+            // 
+            this.lbl.Location = new System.Drawing.Point(12, 9);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(392, 43);
+            this.lbl.TabIndex = 3;
+            this.lbl.Text = "Input:";
+            this.lbl.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // InputBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 115);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "InputBox";
+            this.Text = "InputBox";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnOk;
+        private Button btnCancel;
+        private TextBox tbInput;
+        private Label lbl;
+    }
+}
---
+++ PeridotWindows/EditorScreen/Forms/InputBox.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PeridotWindows.EditorScreen.Forms
+{
+    public partial class InputBox : Form
+    {
+        public InputBox()
+        {
+            InitializeComponent();
+        }
+
+        public static string? Show(string title, string text, string? defaultInputValue)
+        {
+            InputBox frm = new();
+
+            frm.lbl.Text = text;
+            frm.Text = title;
+            frm.tbInput.Text = defaultInputValue ?? "";
+
+            DialogResult result = frm.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                return frm.tbInput.Text;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+    }
+}

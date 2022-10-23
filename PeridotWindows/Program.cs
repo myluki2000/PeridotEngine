2c621694f054d10bb5a7b2b6b968cf3ddd5cc5d5
myluki2000 <lukas636@live.com>
2022-10-23 16:31:09 +0200

    Basic project stuff

--- PeridotWindows/Program.cs
+++ PeridotWindows/Program.cs
@@ -1,14 +1,25 @@
-﻿using System;
+using PeridotEngine;
+using PeridotEngine.Graphics;
 
 namespace PeridotWindows
 {
-    internal class Program
+    internal static class Program
     {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
         [STAThread]
         static void Main()
         {
-            using (var game = new PeridotEngine.Main())
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            //Application.Run(new MainForm());
+            using (var game = new Main())
+            {
+                ScreenManager.CurrentScreen = new EditorScreen.EditorScreen();
                 game.Run();
+            }
         }
     }
 }

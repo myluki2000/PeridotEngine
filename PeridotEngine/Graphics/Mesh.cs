e19c796a5ab05b89bc90431d8ebe84bbb82c6674
myluki2000 <lukas636@live.com>
2022-10-23 16:30:27 +0200

    Add mesh class

---
+++ PeridotEngine/Graphics/Mesh.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PeridotEngine.Graphics
+{
+    internal class Mesh
+    {
+        public VertexPositionColorTexture[] Vertices { get; set; }
+        public uint TextureId { get; set; }
+    }
+}

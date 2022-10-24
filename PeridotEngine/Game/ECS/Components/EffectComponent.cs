7d5725e607a9d7b8d0720e87d415bf7db134d616
myluki2000 <lukas636@live.com>
2022-10-24 13:19:49 +0200

    Add EffectComponent

---
+++ PeridotEngine/Game/ECS/Components/EffectComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PeridotEngine.Game.ECS.Components
+{
+    public class EffectComponent : IComponent
+    {
+        public Effect Effect { get; set; }
+
+        public EffectComponent(Effect effect)
+        {
+            Effect = effect;
+        }
+    }
+}

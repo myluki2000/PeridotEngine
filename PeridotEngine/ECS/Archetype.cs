84f6b38c515d9e95b2ecc52a7ab5a70e650a60d3
myluki2000 <lukas636@live.com>
2022-11-23 21:06:42 +0100

    ECS: Only create one entity object per entity

--- PeridotEngine/ECS/Archetype.cs
+++ PeridotEngine/ECS/Archetype.cs
@@ -13,6 +13,8 @@ namespace PeridotWindows.ECS
         public List<string?> Names { get; } = new();
         public List<IList> Components { get; } = new();
 
+        private readonly List<WeakReference<Entity>?> entities;
+
         public event Action? EntityListChanged;
 
         public Archetype(Type[] componentTypes, List<uint> ids, List<string?> names, List<IList> components) : this(componentTypes)
@@ -20,27 +22,56 @@ namespace PeridotWindows.ECS
             Ids = ids;
             Names = names;
             Components = components;
+
+            entities = new();
+            for (int i = 0; i < ids.Count; i++)
+            {
+                entities.Add(null);
+            }
+
         }
 
         public Archetype(Type[] componentTypes)
         {
             ComponentTypes = componentTypes.OrderBy(x => x.FullName).ToArray();
+
+            entities = new();
         }
 
         public Entity EntityById(uint entityId)
         {
-            return new Entity(entityId, this);
+            int index = Ids.IndexOf(entityId);
+
+            return EntityAt(index);
         }
 
         public Entity EntityAt(int index)
         {
-            return new Entity(Ids[index], this);
+            if (entities[index]?.TryGetTarget(out Entity? entity) ?? false)
+                return entity;
+
+            entity = Entity.FromIndex(index, this);
+            entities[index] = new WeakReference<Entity>(entity);
+
+            return entity;
         }
 
         public void RemoveEntityAt(int index)
         {
+            if (entities[index]?.TryGetTarget(out Entity? entity) ?? false)
+            {
+                if (!entity.IsDeleted)
+                {
+                    entity.Delete();
+                    // entity.Delete() will call this function again anyways, so we can just stop here.
+                    return;
+                }
+            }
+
             Ids.RemoveAt(index);
             Names.RemoveAt(index);
+            entities.RemoveAt(index);
+
             foreach (IList list in Components)
             {
                 list.RemoveAt(index);
@@ -55,6 +86,14 @@ namespace PeridotWindows.ECS
 
         public void CreateEntity(string? name, params ComponentBase[] entityComponents)
         {
+            // check that all components of the archetype were provided for the entity
+            if (entityComponents.Length != Components.Count) throw new Exception("Not all required components were passed to the CreateEntity method when creating an entity of this specific archetype.");
+            foreach (Type componentType in ComponentTypes)
+            {
+                if (!entityComponents.Any(e => e.GetType() == componentType))
+                    throw new Exception("Not all required components were passed to the CreateEntity method when creating an entity of this specific archetype.");
+            }
+
             uint id = Ids.Count > 0
                 ? Ids.Max() + 1
                 : 0;
@@ -66,20 +105,14 @@ namespace PeridotWindows.ECS
 
             entityComponents = entityComponents.OrderBy(x => x.GetType().FullName).ToArray();
 
-            // check that all components of the archetype were provided for the entity
-            if (entityComponents.Length != Components.Count) throw new Exception("Not all required components were passed to the CreateEntity method when creating an entity of this specific archetype.");
-            foreach(Type componentType in ComponentTypes)
-            {
-                if(!entityComponents.Any(e => e.GetType() == componentType))
-                    throw new Exception("Not all required components were passed to the CreateEntity method when creating an entity of this specific archetype.");
-            }
-
             Names.Add(name);
      
             for (int i = 0; i < entityComponents.Length; i++)
             {
                 Components[i].Add(entityComponents[i]);
             }
+
+            entities.Add(null);
 
             EntityListChanged?.Invoke();
         }
@@ -89,7 +122,13 @@ namespace PeridotWindows.ECS
         {
             for (int i = 0; i < EntityCount; i++)
             {
-                yield return new Entity(Ids[i], this);
+                if (entities[i]?.TryGetTarget(out Entity? entity) ?? false)
+                    yield return entity;
+
+                entity = Entity.FromIndex(i, this);
+                entities[i] = new WeakReference<Entity>(entity);
+
+                yield return entity;
             }
         }
 
